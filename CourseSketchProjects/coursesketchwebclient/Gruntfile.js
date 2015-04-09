5d9a0a4f1cde11a810a3360a770aeabebcb7d0c9
dtracers <dtracers@gmail.com>
2015-04-08 19:40:25 -0500

    added a regex that adds validateFirstRun to top
    This added that to the top of every single bower component as of right
    now
    
    
    Former-commit-id: 3f83c5bcbeba816084c0eef64edb4d02fd3f59e9

--- CourseSketchProjects/coursesketchwebclient/Gruntfile.js
+++ CourseSketchProjects/coursesketchwebclient/Gruntfile.js
@@ -205,6 +205,19 @@ module.exports = function(grunt) {
                     }
                 ]
             },
+            // TODO: change this into a plugin
+            runOncePlugins: {
+                src: [ 'target/website/bower_components/jquery/dist/jquery.js', 'target/website/bower_components/babel-polyfill/browser-polyfill.js',
+                        'target/website/bower_components/webcomponentsjs/webcomponents.js' ],
+                overwrite: true,
+                replacements: [
+                    {
+                        // addes bower comment
+                        from: /^/,
+                        to: 'validateFirstRun(document.currentScript);'
+                    }
+                ]
+            },
             appEngine: {
                 src: [ 'target/website/app.yaml' ],
                 overwrite: true,
@@ -352,7 +365,8 @@ module.exports = function(grunt) {
         grunt.task.run([
             'replace:bowerLoad',
             'wiredep',
-            'replace:bowerSlash'
+            'replace:bowerSlash',
+            'replace:runOncePlugins'
         ]);
     });
 
