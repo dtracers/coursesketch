8417e820aa5129d62c3b9c0523a81f45bf815e63
sketchrec <sketchrec@googlegroups.com>
2015-05-12 19:42:14 -0500

    Added checks to the update scripts
    Former-commit-id: 8d96c9a6c1e7af42996200996c5b10cfb8784992

--- config/dev_info/github_auto/buildupdate.sh
+++ config/dev_info/github_auto/buildupdate.sh
@@ -4,12 +4,24 @@ cd dev_info
 cd github_auto
 
 /bin/bash automerge.sh
+success=$?
+if [[ $success -eq 0 ]];
+then
+    echo "updating branches Successful"
+else
+    echo "merging failed"
+    exit 1
+fi
 cd ../../../
 mvn clean install
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
+    echo "install Successful"
+else
+    echo "Maven Failed"
+    exit 1
+fi
 
-echo "install Successful"
 cd config
 
 /bin/bash copyjars.sh <<< "/home/sketchlab/VirtualBox VMs/Shared/" 
@@ -22,8 +34,3 @@ scp -r "/home/sketchlab/VirtualBox VMs/Shared/coursesketch/CourseSketchProjects/
 
 ssh hammond@goldberglinux01.tamu.edu  
 
-else
-
-echo "Maven Failed"
-
-fi
