5235d76391887ac80149e76f8eb4959fae75e6ed
sketchrec <sketchrec@googlegroups.com>
2015-05-12 19:24:09 -0500

    Added changes to the build update file
    Former-commit-id: eee4089d59a65d50a1fa63ae9317f53690d861b7

--- config/dev_info/github_auto/buildupdate.sh
+++ config/dev_info/github_auto/buildupdate.sh
@@ -6,6 +6,24 @@ cd github_auto
 /bin/bash automerge.sh
 cd ../../../
 mvn clean install
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+
+echo "install Successful"
 cd config
 
 /bin/bash copyjars.sh <<< "/home/sketchlab/VirtualBox VMs/Shared/" 
+
+mv "/home/sketchlab/VirtualBox VMs/Shared/coursesketch/CourseSketchProjects/coursesketchwebclient/target/website" "/home/sketchlab/VirtualBox VMs/Shared/coursesketch/CourseSketchProjects/coursesketchwebclient/target/coursesketchwebclient"
+
+cp -r "/home/sketchlab/VirtualBox VMs/Shared/coursesketch/CourseSketchProjects/coursesketchwebclient/target/coursesketchwebclient" "~/coursesketch/coursesketchwebclient/"
+
+scp -r "/home/sketchlab/VirtualBox VMs/Shared/coursesketch/CourseSketchProjects/coursesketchwebclient/target/coursesketchwebclient" hammond@goldberglinux01.tamu.edu:local
+
+ssh hammond@goldberglinux01.tamu.edu  
+
+else
+
+echo "Maven Failed"
+
+fi
